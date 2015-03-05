dada7a34557d6b16ff84ea381f59473ccac12c69
MichaelByrd <michaelbyrdsemail@gmail.com>
2015-03-04 23:19:02 -0500

    finally somewhat readable code

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,6 +4,10 @@ class Profile < ActiveRecord::Base
   HEADER = {:headers => {"Authorization" => "token #{ENV['GITHUB_TOKEN']}",
                "User-Agent" => "anyone"}}
 
+  def update?
+   (DateTime.now.to_i - updated_at.to_i) > 1.day
+  end
+
   def repos_response
     HTTParty.get(self.repos_url, HEADER)
   end
@@ -12,49 +16,36 @@ class Profile < ActiveRecord::Base
     HTTParty.get("https://api.github.com/users/#{self.username}", HEADER)
   end
 
-
   def self.get_updated_profile(username)
     profile = Profile.find_by(username: username) ||
               Profile.create_from_api(username)
-
     profile.update_if_needed
-  end
-
-  def update_from_api
-    response = profile_response
-
-    self.update(
-      username: username,
-      repos_url: response["repos_url"],
-      avatar_url: response["avatar_url"],
-      location: response["location"],
-      company_name: response["company"],
-      number_of_followers: response["followers"].to_i,
-      number_following: response["following"].to_i,
-      github_updated_at: response["updated_at"].to_datetime
-    )
   end
 
   def self.create_from_api(username)
     response = HTTParty.get("https://api.github.com/users/#{username}", HEADER)
     if response["login"] == username
-      profile = Profile.create(
-        username: username,
-        repos_url: response["repos_url"],
-        avatar_url: response["avatar_url"],
-        location: response["location"],
-        company_name: response["company"],
+      profile = Profile.create( username: username,
+        repos_url: response["repos_url"], avatar_url: response["avatar_url"],
+        location: response["location"], company_name: response["company"],
         number_of_followers: response["followers"].to_i,
         number_following: response["following"].to_i,
-        github_updated_at: response["updated_at"].to_datetime
-      )
+        github_updated_at: response["updated_at"].to_datetime )
       Repository.create_or_update_from_api(profile)
       profile
-    else
-      raise
     end
   end
 
+
+  def update_from_api
+    response = profile_response
+    self.update(username: username,
+      repos_url: response["repos_url"], avatar_url: response["avatar_url"],
+      location: response["location"], company_name: response["company"],
+      number_of_followers: response["followers"].to_i,
+      number_following: response["following"].to_i,
+      github_updated_at: response["updated_at"].to_datetime )
+  end
 
   def update_if_needed
     if self.update?
@@ -64,10 +55,6 @@ class Profile < ActiveRecord::Base
       self.update_repos_from_api
     end
     self
-  end
-
-  def update?
-    (DateTime.now.to_i - updated_at.to_i) > 1.day
   end
 
   def update_repos?
--- app/models/repository.rb
+++ app/models/repository.rb
@@ -8,15 +8,17 @@ class Repository < ActiveRecord::Base
     HTTParty.get( self.url, HEADER )
   end
 
+  def update?
+    (DateTime.now.to_i - updated_at.to_i) > 2.hours
+  end
+
   def update_from_api
     response = repo_response
     if response["message"] =="Not Found"
       self.destroy!
     else
-      self.update(
-        name: response["name"],
-        url: response["url"],
-        html_url: response["html_url"],
+      self.update( name: response["name"],
+        url: response["url"], html_url: response["html_url"],
         number_of_forks: response["forks_count"].to_i,
         number_of_stars: response["stargazers_count"].to_i,
         github_updated_at: response["updated_at"].to_datetime,
@@ -38,34 +40,25 @@ class Repository < ActiveRecord::Base
   end
 
   def update_from_hash(hash)
-    self.update(
-      name: hash["name"],
-      url: hash["url"],
-      html_url: hash["html_url"],
+    self.update( name: hash["name"], language: hash["language"],
+      url: hash["url"], html_url: hash["html_url"],
       number_of_forks: hash["forks_count"].to_i,
       number_of_stars: hash["stargazers_count"].to_i,
-      github_updated_at: hash["updated_at"].to_datetime,
-      language: hash["language"]
+      github_updated_at: hash["updated_at"].to_datetime
+
     )
   end
 
   def self.create_from_hash(hash)
     profile = Profile.find_by(username: hash["owner"]["login"])
-    Repository.create(
-      profile: profile,
-      name: hash["name"],
-      url: hash["url"],
+    Repository.create( profile: profile, language: hash["language"],
+      name: hash["name"], url: hash["url"],
       html_url: hash["html_url"],
       number_of_forks: hash["forks_count"].to_i,
       number_of_stars: hash["stargazers_count"].to_i,
       github_updated_at: hash["updated_at"].to_datetime,
-      github_id: hash["id"].to_i,
-      language: hash["language"]
+      github_id: hash["id"].to_i
     )
-  end
-
-  def update?
-    (DateTime.now.to_i - updated_at.to_i) > 2.hours
   end
 
 end
